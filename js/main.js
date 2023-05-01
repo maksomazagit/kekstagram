8975c452cf501238b02a4c1000c1522ba1cc78d6
maksomazagit <maksomazagit@gmail.com>
2023-05-01 14:44:58 +0100

    2 fn added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-function Name () {}
+// Random numbers function
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    if (max < min) {
+        const temp = max;
+        max = min;
+        min = temp;
+    }
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//Max string length function
+function checkMaxLength(str, maxLen) {
+    if (str.length <= maxLen) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+getRandomInt(2, 15);
+checkMaxLength('string', 5);
